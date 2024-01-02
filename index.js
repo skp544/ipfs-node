d1d1dba752e0549a9135b8a62848119b3533b69d
Shubham Kumar Prajapati <91832644+skp544@users.noreply.github.com>
2024-01-02 11:49:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
-import express from "express"
-import fileupload from "express-fileupload"
-import cors from "cors"
+import express from "express";
+import multer from "multer";
+import cors from "cors";
+import path from "path";
+
+const storage = multer.diskStorage({
+    destination: "../storage/",
+    filename: function (req, file, cb) {
+      cb(
+        null,
+        file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+      );
+    },
+  });
+  const upload = multer({
+    storage: storage,
+  }).single("file");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+// Serve static files from the "storage" directory
+app.use("../storage", express.static("storage"));
 
 
-
-
-const app  =express()
-
-app.use(express.urlencoded())
-app.use(express.json())
-app.use(cors())
-app.use(fileupload())
-
-app.post("/file", async (req,res) => {
+app.post("/upload", async (req, res) => {
     try {
-        
-        res.json({files: req.files.file})
+        upload(req, res, (err) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({ error: "File upload failed" });
+            }
+            res.json({ file: req.file });
+        });
     } catch (error) {
-        
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
     }
-})
+});
 
+const PORT = 5000;
 
-app.listen(5000, ()=>{
-    console.log("Server is running on 5000")
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
